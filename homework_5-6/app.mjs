4e200d1cb48c2ac985617fa32f968d05184086db
Kr1st0ffers0n <romakolishko@gmail.com>
2024-10-28 11:52:30 +0200

    Update app.mjs

--- homework_5-6/app.mjs
+++ homework_5-6/app.mjs
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
 // error handler 
 app.get('/', (req, res, next) => {
     res.redirect('/cars/home'); // або рендерте відповідний файл views/index
+    next();
 });
 app.use((err, req, res, next) => {
     // set locals, only providing error in development 
