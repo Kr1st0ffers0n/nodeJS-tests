deb026b3a2c936c75baa429839cfaeba00e04e00
Kr1st0ffers0n <romakolishko@gmail.com>
2024-12-14 17:58:18 +0200

    Delete homework_9 directory

--- homework_9/config/session.mjs
+++
@@ -1,9 +0,0 @@
-import session from 'express-session'
-import config from './default.mjs'
-const sessionConfig = session({
-  secret: config.secretKey,
-  resave: false,
-  saveUninitialized: false,
-})
-
-export default sessionConfig
